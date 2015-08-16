bbc2c9156b37031b195161db385709e24ecf8b86
bastiendavid <bastien.david.info@gmail.com>
2015-08-16 18:04:36 +0200

    Updated HelpActivity text + style
    New text for HelpActivity, and minimalist styling.

--- app/src/main/java/com/orange/androidlocales/HelpActivity.java
+++ app/src/main/java/com/orange/androidlocales/HelpActivity.java
@@ -1,6 +1,11 @@
 package com.orange.androidlocales;
 
+import android.text.method.LinkMovementMethod;
+import android.widget.TextView;
+
+import org.androidannotations.annotations.AfterViews;
 import org.androidannotations.annotations.EActivity;
+import org.androidannotations.annotations.ViewById;
 
 /**
  * Help activity. Run by default, when the application is launched, displays some documentation on how to use this application.
@@ -8,4 +13,11 @@ import org.androidannotations.annotations.EActivity;
 @EActivity(R.layout.help_activity)
 public class HelpActivity extends BaseActivity {
 
+    @ViewById(R.id.more_help)
+    TextView moreHelpView;
+
+    @AfterViews
+    void activateMoreHelpLink() {
+        moreHelpView.setMovementMethod(LinkMovementMethod.getInstance());
+    }
 }
