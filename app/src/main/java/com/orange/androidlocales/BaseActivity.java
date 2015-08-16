777399a1d215fa877fe4a4c784125afcb453c160
bastiendavid <bastien.david.info@gmail.com>
2015-08-16 13:07:24 +0200

    Initial commit
    Added base code to run a hello world activity

---
+++ app/src/main/java/com/orange/androidlocales/BaseActivity.java
@@ -0,0 +1,9 @@
+package com.orange.androidlocales;
+
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by Bastien on 16/08/2015.
+ */
+public class BaseActivity extends AppCompatActivity {
+}
---
+++ app/src/main/java/com/orange/androidlocales/HelpActivity.java
@@ -0,0 +1,13 @@
+package com.orange.androidlocales;
+
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+import org.androidannotations.annotations.EActivity;
+
+@EActivity(R.layout.help_activity)
+public class HelpActivity extends BaseActivity {
+
+}
